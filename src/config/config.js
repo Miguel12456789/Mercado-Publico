7b8042c95a2807849dd6763332121df130c0570e
Miguel12456789 <159816052+Miguel12456789@users.noreply.github.com>
2025-07-04 18:28:39 +0100

    a

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,28 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        console.log("MONGO_URI:", process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        console.log("MONGO_URI_1:", process.env.MONGO_URI_1);
+        const conn1 = mongoose.createConnection(process.env.MONGO_URI_1);
+        await new Promise((resolve, reject) => {
+            conn1.once('connected', resolve);
+            conn1.once('error', reject);
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Cluster 1 Connected: ${conn1.db.databaseName}`);
+
+        console.log("MONGO_URI_2:", process.env.MONGO_URI_2);
+        const conn2 = mongoose.createConnection(process.env.MONGO_URI_2);
+        await new Promise((resolve, reject) => {
+            conn2.once('connected', resolve);
+            conn2.once('error', reject);
+        });
+        console.log(`MongoDB Cluster 2 Connected: ${conn2.db.databaseName}`);
+
+        // Retorne as conexões para uso posterior
+        return { conn1, conn2 };
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
-
 
 module.exports = connectDB;
